58f75c3402414b514242e28e84ae5497d0e93131
DaniilGalt <94684997+GaltDaniil@users.noreply.github.com>
2023-03-04 08:54:34 +0800

    изменил axios на /app

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://95.163.242.251:4444',
+    baseURL: 'https://balimood.ru/app',
 });
 
 instance.interceptors.request.use((config) => {
