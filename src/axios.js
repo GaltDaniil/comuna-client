82d891431d9e32f10218456777c6ac8715e4e202
DaniilGalt <94684997+GaltDaniil@users.noreply.github.com>
2023-03-04 10:22:23 +0800

    удалил /app

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://balimood.ru/app',
+    baseURL: 'https://balimood.ru',
 });
 
 instance.interceptors.request.use((config) => {
