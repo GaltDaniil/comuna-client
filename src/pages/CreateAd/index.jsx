1852d327da5e97e12398233e5fd53b23814960a9
DaniilGalt <94684997+GaltDaniil@users.noreply.github.com>
2023-03-06 00:28:54 +0800

    мучаю селект и страницу создание товара

--- src/pages/CreateAd/index.jsx
+++ src/pages/CreateAd/index.jsx
@@ -15,11 +15,26 @@ import {
     IconButton,
 } from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
+import { useForm, Controller } from 'react-hook-form';
+
+import ReactSelect from 'react-select';
+
+const categories = [
+    { value: '0', label: 'category 1' },
+    { value: '1', label: 'category 2' },
+    { value: '2', label: 'category 3' },
+    { value: '3', label: 'category 4' },
+    { value: '4', label: 'category 5' },
+    { value: '5', label: 'category 6' },
+    { value: '6', label: 'category 7' },
+];
+
+const getValue = (value) => (value ? categories.find((option) => option.value === value) : '');
 
 export const CreateAd = () => {
     const [category, setCategory] = React.useState('');
-    const [isNew, setIsNew] = React.useState('0');
-    const [isСurrency, setIsСurrency] = React.useState('RP');
+    const [isCondition, setIsCondition] = React.useState('old');
+    const [isСurrency, setIsСurrency] = React.useState('USD');
 
     const changeCategory = (event) => {
         setCategory(event.target.value);
@@ -28,33 +43,53 @@ export const CreateAd = () => {
         setIsСurrency(event.target.value);
     };
     const changeNewOrOld = (event, newAlignment) => {
-        setIsNew(newAlignment);
+        setIsCondition(newAlignment);
+    };
+    const onSubmit = (props) => {
+        console.log(props);
     };
 
-    /*  const useStyles = makeStyles({
-        customTextField: {
-            '& input::placeholder': {
-                fontSize: '20px',
-            },
+    const {
+        register,
+        handleSubmit,
+        setError,
+        control,
+        formState: { errors, isValid },
+    } = useForm({
+        defaultValues: {
+            title: '',
+            description: '',
+            categoru: '',
+            condition: 'old',
+            price: '',
+            currency: 'RUB',
+            imagesUrl: [],
+            description: '',
+            description: '',
         },
-    }); */
+        mode: 'onChange',
+    });
 
     return (
         <>
             <Header />
             <div className="styles maincontainer">
-                <div className={styles.container}>
+                <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
                     <h2>Создать новое объявление</h2>
                     <div>
                         <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
                             <h3 className={styles.title}>Название объявления</h3>
                             <TextField
+                                {...register('title', {
+                                    required: 'Укажите заголовок',
+                                    maxLength: { value: 220, message: 'Не более 220 символов' },
+                                })}
+                                required
+                                helperText={errors.title?.message}
+                                error={Boolean(errors.title?.message)}
                                 fullWidth
-                                id="outlined-basic"
                                 label="Заголовок объявления"
                                 variant="outlined"
-                                multiline=""
-                                helperText="не более 200 символов"
                             />
                         </div>
                     </div>
@@ -66,8 +101,14 @@ export const CreateAd = () => {
                                 fullWidth
                                 multiline
                                 rows={4}
+                                {...register('description', {
+                                    required: 'Укажите описание товара',
+                                    maxLength: { value: 2200, message: 'Не более 2200 символов' },
+                                })}
+                                required
+                                helperText={errors.description?.message}
+                                error={Boolean(errors.description?.message)}
                                 label="Подробное описание вашего товара"
-                                helperText="не более 2200 символов"
                             />
                         </div>
                     </div>
@@ -75,8 +116,8 @@ export const CreateAd = () => {
                         <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
                             <h3 className={styles.title}>Категория</h3>
                             <FormControl sx={{ minWidth: 200, width: 210 }}>
-                                <InputLabel id="demo-simple-select-label">Категория</InputLabel>
-                                <Select
+                                {/* <InputLabel id="demo-simple-select-label">Категория</InputLabel> */}
+                                {/* <Select
                                     labelId="demo-simple-select-label"
                                     placeholder="Ваша категория"
                                     id="demo-simple-select"
@@ -84,13 +125,34 @@ export const CreateAd = () => {
                                     label="Категория"
                                     onChange={changeCategory}
                                 >
-                                    <MenuItem value={1}>Одежда, обувь и аксессуары </MenuItem>
-                                    <MenuItem value={2}>Электроника и аксессуары</MenuItem>
-                                    <MenuItem value={3}>Для дома</MenuItem>
-                                    <MenuItem value={4}>Красота и здоровье</MenuItem>
-                                    <MenuItem value={5}>Хобби и отдых</MenuItem>
-                                    <MenuItem value={6}>Другое</MenuItem>
-                                </Select>
+                                    <MenuItem value={'1'}>Одежда, обувь и аксессуары </MenuItem>
+                                    <MenuItem value={'2'}>Электроника и аксессуары</MenuItem>
+                                    <MenuItem value={'3'}>Для дома</MenuItem>
+                                    <MenuItem value={'4'}>Красота и здоровье</MenuItem>
+                                    <MenuItem value={'5'}>Хобби и отдых</MenuItem>
+                                    <MenuItem value={'6'}>Другое</MenuItem>
+                                </Select> */}
+                                <Controller
+                                    control={control}
+                                    name="category"
+                                    rules={{
+                                        required: 'Выберите категорию',
+                                    }}
+                                    error={errors.description?.message}
+                                    render={({
+                                        field: { onChange, value },
+                                        fieldState: { error },
+                                    }) => (
+                                        <div>
+                                            <ReactSelect
+                                                placeholder="Категории"
+                                                options={categories}
+                                                value={getValue(value)}
+                                                onChange={(value) => onChange(value).value}
+                                            />
+                                        </div>
+                                    )}
+                                />
                             </FormControl>
                         </div>
                     </div>
@@ -99,7 +161,11 @@ export const CreateAd = () => {
                             <h3 className={styles.title}>Состояние</h3>
                             <ToggleButtonGroup
                                 color="primary"
-                                value={isNew}
+                                {...register('condition', {
+                                    required: 'Укажите описание товара',
+                                })}
+                                value={isCondition}
+                                required
                                 exclusive
                                 onChange={changeNewOrOld}
                                 aria-label="Platform"
@@ -114,6 +180,13 @@ export const CreateAd = () => {
                             <h3 className={styles.title}>Цена</h3>
                             <TextField
                                 sx={{ minWidth: 200, width: 210 }}
+                                {...register('price', {
+                                    required: 'Укажите цену',
+                                    maxLength: { value: 12, message: 'Не более 12 цифр' },
+                                })}
+                                required
+                                helperText={errors.price?.message}
+                                error={Boolean(errors.price?.message)}
                                 id="outlined-basic"
                                 label="Cумма"
                                 variant="outlined"
@@ -122,27 +195,30 @@ export const CreateAd = () => {
                             <FormControl sx={{ minWidth: 50, width: 70 }}>
                                 <InputLabel id="demo-simple-select-label">Валюта</InputLabel>
                                 <Select
-                                    labelId="demo-simple-select-label"
-                                    placeholder="Ваша категория"
-                                    id="demo-simple-select"
-                                    value={isСurrency}
                                     label="Валюта"
                                     onChange={changeСurrency}
-                                    helperText="выберете валюту"
+                                    {...register('currency', {
+                                        value: 'RUB',
+                                        required: 'Укажите валюту',
+                                    })}
+                                    required
+                                    helperText={errors.currency?.message}
+                                    error={Boolean(errors.currency?.message)}
                                 >
-                                    <MenuItem label="$" value={'USD'}>
+                                    <MenuItem label="$" value="USD">
                                         $
                                     </MenuItem>
-                                    <MenuItem label="₽" value={'RUB'}>
+                                    <MenuItem label="₽" value="RUB">
                                         ₽
                                     </MenuItem>
-                                    <MenuItem label="RS" value={'RS'}>
+                                    <MenuItem label="RS" value="RS">
                                         ₨
                                     </MenuItem>
                                 </Select>
                             </FormControl>
                         </div>
                     </div>
+
                     <div>
                         <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
                             <h3 className={styles.title}>Фотографии</h3>
@@ -159,10 +235,11 @@ export const CreateAd = () => {
                     <Button
                         sx={{ mt: 5, width: 180, height: 40, background: '#00aa5b' }}
                         variant="contained"
+                        type="submit"
                     >
                         Опубликовать
                     </Button>
-                </div>
+                </form>
             </div>
         </>
     );
